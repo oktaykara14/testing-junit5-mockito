fbc21399af38814832cd43257ae1bfb60ee10d0f
oktaykara <oktaykara14@gmail.com>
2023-11-08 15:55:34 +0300

    .

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -45,6 +45,7 @@ class VisitSDJpaServiceTest {
         assertThat(foundVisits).hasSize(1);
     }
 
+    @DisplayName("Test Find By Id")
     @Test
     void findById() {
         Visit visit = new Visit();
